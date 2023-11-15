9845fd06a8cc323db60f2a195e6f3acf9a8565df
MiguelR222 <miguel.ruiz0652@gmail.com>
2023-11-14 22:53:49 -0600

    Calculator app created using reactnative

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
+import {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch } from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
+import { myColors } from './src/styles/colors';
+import MyKeyboard from './src/components/MyKeyboard';
+import { SafeAreaView } from 'react-native';
 
 export default function App() {
+  const [theme, setTheme] =useState('light');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <ThemeContext.Provider value={theme}>
+    <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: myColors.black}]}>
       <StatusBar style="auto" />
-    </View>
+      <Switch 
+      value= {theme === 'light'}
+      onValueChange={()=> setTheme(theme === 'light' ? 'dark' : 'light')}
+      />
+        <MyKeyboard/>
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: myColors.light,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
 });
